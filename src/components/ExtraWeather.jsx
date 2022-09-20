576ba5aa35be746ddfbadd7240228d2d4f38f2e1
unknown <OverflowDev>
2022-09-20 20:43:39 +0100

    mobile device

--- src/components/ExtraWeather.jsx
+++ src/components/ExtraWeather.jsx
@@ -4,32 +4,30 @@ import weather from '../assets/weather-moon-svgrepo-com.svg'
 
 function ExtraWeather({loc}) {
   return (
-    <div className='flex justify-center gap-3 md:p-12 md:my-0 my-12 p-4'>
-      <div className=''>
-        <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
-          <img src={wind} alt="wind" className='h-12 w-12' />
-          {loc.wind ? 
-            <p className='font-bold text-black tracking-widest leading-7'>{loc.wind.speed} MPH</p>
-            :
-            null
-          }
-        </div>
-        <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
-          <img src={humidity} alt="humidity" className='h-12 w-12' />
-          {loc.main ? 
-            <p className='font-bold text-black tracking-widest leading-7'>{loc.main.humidity} %</p>
-            :
-            null
-          }
-        </div>
-        <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
-          <img src={weather} alt="feels like" className='h-12 w-12' />
-          {loc.main ? 
-            <p className='font-bold text-black tracking-widest leading-7'>{loc.main.feels_like.toFixed()} °</p>
-            :
-            null
-          }
-        </div>
+    <div className='flex justify-center space-x-4 md:p-12 md:my-0 my-12 p-4'>
+      <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
+        <img src={wind} alt="wind" className='h-12 w-12' />
+        {loc.wind ? 
+          <p className='font-bold text-black tracking-widest leading-7'>{loc.wind.speed} MPH</p>
+          :
+          null
+        }
+      </div>
+      <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
+        <img src={humidity} alt="humidity" className='h-12 w-12' />
+        {loc.main ? 
+          <p className='font-bold text-black tracking-widest leading-7'>{loc.main.humidity} %</p>
+          :
+          null
+        }
+      </div>
+      <div className='bg-blue-300 rounded-lg shadow-md shadow-inherit p-4 w-48 flex flex-col justify-center items-center'>
+        <img src={weather} alt="feels like" className='h-12 w-12' />
+        {loc.main ? 
+          <p className='font-bold text-black tracking-widest leading-7'>{loc.main.feels_like.toFixed()} °</p>
+          :
+          null
+        }
       </div>
     </div>
   )
